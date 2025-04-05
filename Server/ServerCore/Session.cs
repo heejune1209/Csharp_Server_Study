5a8021655fb66acaacd6bbf5d17fc5b2daedbeec
heejune <heejune1209@gmail.com>
2025-04-05 13:20:51 +0900

    commit

--- Server/ServerCore/Session.cs
+++ Server/ServerCore/Session.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
@@ -9,102 +10,137 @@ namespace ServerCore
     {
         Socket _socket;
         int _disconnected = 0;
+        object _lock = new object();
+        // send를 보낼 때 쌓아놓고 한번에 보내는 용도
+        Queue<byte[]> _sendQueue = new Queue<byte[]>();
+
+        // 일단 한번 Register를 했다면 Send가 완료될 때까지 대기를 할 수 있도록 해줌
+        // 누군가가 Send를 하고 있다면 Register를 하는 것이 아니라 Queue에다가 sendbuff를 쌓아 놓게 됨
+        bool _pending = false;
+        SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+
         public void Start(Socket socket)
         {
             _socket = socket;
             SocketAsyncEventArgs recvArgs = new SocketAsyncEventArgs();
             recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvComplected);
+            recvArgs.SetBuffer(new byte[1024], 0, 1024);
+            _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendComplected);
 
-            // 유저에 대한 정보를 아무거나 전달해줘도 된다. => 식별자로 구분하고 싶거나 연동하고 싶을 때 사용
-            // recvArgs.UserToken = 1; // 아니면 이 세션에서 온 애라는 뜻으로 this를 넣어줘도 된다.
-            // 이건 Listener 전용일 때 사용
-            // recvArgs.AcceptSocket
-
-            // recv를 할 때 buffer를 만들어줬던 것처럼 아래와 같이 SetBuffer를 해야한다.
-            // offset이 0이 아닌 경우도 있음. -> 버퍼에 큰 값을 할당해오고 세션별로 쪼개서 가지고 가는 경우가 있기 때문
-            // 하지만 지금은 세션을 만들때마다 버퍼를 새로 만들어주기 때문에 offset은 0으로 해도 된다.
-            recvArgs.SetBuffer(new byte[1024], 0, 1024); // 버퍼, 버퍼를 시작하는 위치, 버퍼의 길이
-
-            // Receive 시작
             RegisterRecv(recvArgs);
         }
 
         public void Send(byte[] sendBuff)
-        {
-            _socket.Send(sendBuff);
+        {  
+            // lock을 잡아서 한번에 한명씩만 진입 할 수 있도록 해준다.
+            lock (_lock)
+            {
+                _sendQueue.Enqueue(sendBuff);
+                // 아직 아무도 send를 하지 않은 상태
+                // send큐에 sendBuff를 넣어준 다음 pending이 false인 상태라면(첫번째라면) RegisterSend 호출
+                if (_pending == false) // true면 누군가는 send를 하고 있는 상태
+                {                                       
+                    RegisterSend();
+                }
+            }
         }
-        // 동시다발적으로 Disconnect를 하는 경우 
-        // Disconnect를 같은 사람이 2번 하게 된다면?
-        // 오류가 발생
-        // Disconnect를 한번만 하도록 작업
+
+        // recv 같은 경우는 처음에 예약을 걸어놓고 클라이언트에서 요청이 들어올 때까지 대기를 타면 됐지만
+        // send는 처음에 예약을 거는 것 자체가 말이 되지가 않는다 => 무엇을 보낼지 어떻게 알고 예약을 걸 수 있을까?
+        // Send에서 이미 lock 처리를 했기 때문에 굳이 여기서는 lock처리를 할 필요가 없다.
+        void RegisterSend()
+        {
+            _pending = true; 
+            byte[] buff = _sendQueue.Dequeue();
+            _sendArgs.SetBuffer(buff, 0, buff.Length);
+
+            bool pending = _socket.SendAsync(_sendArgs);
+            if (pending == false)
+            {
+                OnSendComplected(null, _sendArgs);
+            }
+        }
+        
+        // 하지만 여기서는 RegisterSend에서 호출될 수도 있지만 이벤트 콜백으로 호출이 될 수도 있기 때문에
+        // lock을 걸어줘야 한다.
+        private void OnSendComplected(object send, SocketAsyncEventArgs args)
+        {
+            lock(_lock)
+            {
+                if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+                {
+                    try
+                    {
+                        // 그 동안 쌓아놓은 Queue가 있다.
+                        // => 누군가가 send를 하는 동안 sendQueue에 buff를 저장해놓고 있었다.
+                        // 그것을 가지고 와서 Dequeue로 처리를 해준다는 이야기
+                        if (_sendQueue.Count > 0)
+                        {
+                            RegisterSend();
+                        }
+                        // 아무도 그 사이에 sendQueue에 패킷을 추가하지 않았다는 이야기
+                        else
+                        {
+                            // Send를 완료했기 때문에 또 다른 Send 등록을 받기 위해서 false로 변경
+                            _pending = false;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        System.Console.WriteLine($"OnSendComplected Failed {e}");
+                    }
+
+                }
+                else
+                {
+                    Disconnect();
+                }   
+            }
+            
+        }
+        // 멀티쓰레드 프로그래밍은 계속 충돌을 내면서 연습을 하는 것이 가장 좋다.
+
+        #region 네트워크 통신
+        private void RegisterRecv(SocketAsyncEventArgs args)
+        {
+            bool pending = _socket.ReceiveAsync(args);
+
+            if (pending == false)
+            {
+                OnRecvComplected(null, args);
+            }
+        }
+
+        private void OnRecvComplected(object sender, SocketAsyncEventArgs args)
+        {
+            if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+            {
+                try
+                {
+                    string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
+                    System.Console.WriteLine($"[From Client : {recvData}");
+
+                    RegisterRecv(args);
+                }
+                catch (System.Exception e)
+                {
+                    System.Console.WriteLine($"OnRecvComplected Failed {e}");
+                }
+
+            }
+            else
+            {
+                Disconnect();
+            }
+        }
+        #endregion
+        
         public void Disconnect()
         {
-            // if (_socket != null)
-            // 이렇게 _socket이 null이 아닐 경우에 Close를 하는 방법은 정상적으로 동작하지 않는다.
-            
-            // Exchange()의 리턴 값은 ref 값이 바뀌기 전의 값이다.
-            // _disconnected == 1 => Disconnect가 완료 되었다(이미 ref값이 1이였다).
-            // 리턴 값이 1이라면 기존에 다른 쓰레드에서 1로 바꿔줬다는 의미
-            // 그래서 return
-            // 즉 Disconnect를 2번 하게 될 때
-            // 이미 한번 Disconnect가 되었기 때문에 
-            // 둘 중에 하나는 if문에 걸려서 동작을 하지 않게 된다.
             if (Interlocked.Exchange(ref _disconnected, 1) == 1)
                 return;
             _socket.Shutdown(SocketShutdown.Both);
             _socket.Close();
         }
-
-        #region 네트워크 통신
-
-        // 비동기 방식으로 처리하기 위해서 2단계로 진행을 함
-        // 등록하는 작업, 완료 후 작업
-        private void RegisterRecv(SocketAsyncEventArgs args)
-        {
-            bool pending = _socket.ReceiveAsync(args);
-
-            // 바로 정보를 받아온 경우
-            if (pending == false)
-            {
-                OnRecvComplected(null, args);
-            }
-            // 만약 pending이 true면 위에 EventHandler를 통해서 OnRecvComplected가 자동으로 콜백으로 실행된다.
-        }
-        private void OnRecvComplected(object sender, SocketAsyncEventArgs args)
-        {
-            // args.BytesTransferred(전달 받은 byte 값) ==  0일 경우가 있다 => 상대방이 접속을 끊는 경우에
-            // 따라서 반드시 0보다 큰 값인지를 체크해야한다. 
-            // 소켓 에러 메세지도 성공인지 확인
-            if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
-            {
-                try
-                {
-                    // To do
-                    // 나중에는 패킷을 분석하는 로직
-                    string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
-                    Console.WriteLine($"[From Client : {recvData}");
-
-                    RegisterRecv(args);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"OnRecvComplected Failed {e}");
-                }
-
-            }
-            // 실패한 경우 
-            else
-            {
-                // To do Disconnet
-            }
-        }
-        #endregion
-        // 중요한 흐름
-        // 한번은 받는 동작이 끝나야 다음 등록이 진행이 된다.
-        // 클라에서 보낸 패킷을 모두 받기 전까지는 더이상 받지 않겠다는 로직
-        // 하지만 Recv는 간단한데 Send는 간단하지 않다.
-        // 일단 Recv 예약을 건 다음에 실제로 클라이언트가 보낸 메시지를 처리하면 되기 때문에 간단
-        // 하지만 Send는 예약을 하는 게 아니라(Recv를 대기 하는 게 아니라) 원하는 타이밍에 보내야 하기 때문에 
-        // 많이 까다롭다.
     }
 }
--- Server/ServerCore/obj/Debug/net8.0/ServerCore.AssemblyInfo.cs
+++ Server/ServerCore/obj/Debug/net8.0/ServerCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba6f00b862ec3006d8aeef56ddb33a1179f5814f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c50e3889eed1f44ea1ac280d40c483ca328b3a13")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
