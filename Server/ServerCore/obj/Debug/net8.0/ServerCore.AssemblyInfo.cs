4e5186d3926edc4a03e529d51cb5c913344a72d7
heejune <heejune1209@gmail.com>
2025-04-05 21:46:08 +0900

    commit

--- Server/ServerCore/obj/Debug/net8.0/ServerCore.AssemblyInfo.cs
+++ Server/ServerCore/obj/Debug/net8.0/ServerCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c50e3889eed1f44ea1ac280d40c483ca328b3a13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a8021655fb66acaacd6bbf5d17fc5b2daedbeec")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
